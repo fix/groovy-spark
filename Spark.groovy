13c3848462a57768e027b851d786caf0b9d34e33
Francois-Xavier Thoorens <fx.thoorens@gmail.com>
2011-11-21 22:05:15 +0100

    groovier version :)

--- Spark.groovy
+++ Spark.groovy
@@ -1,32 +1,39 @@
-if(args.length<2 && System.in.available()){
-	String sysin=System.in.text
-	if(sysin){
-		args=sysin.split("\n");
-		if(args.length<2){
-			args=sysin.split(" ");
+if(args.length<1){
+	sleep 300 // piping can take long
+	if(System.in.available()){ //needed when you no arg to prevent from hanging
+		String sysin=System.in.text
+		if(sysin){
+			args=sysin.split("\n")
+			if(args.length<2){
+				args=sysin.split(" ")
+			}
 		}
 	}
 }
-if(args.length==0) println "usage: spark 0 4 35 7 9 ..."
-else {
-	String sparks = '▁▂▃▄▅▆▇'
+
+def sparkit(String[] args){
 	double min=Double.MAX_VALUE
 	double max=Double.MIN_VALUE
-	output=""
-	args.each{
-		//println it
-		double candidate = it.toDouble()
-		if(candidate < min) min= candidate
-		if (candidate > max) max = candidate
+	String sparks = "\u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588"
+	
+	def argsnum=args.collect {
+		def num = it.toDouble()
+		if(num < min) min = num
+		if(num > max) max = num
+		num
 	}
-
+	
 	double interval=(max-min)/(sparks.size() - 1)
+	
+	argsnum.collect {
+		sparks[(int) ((it - min) / interval)]
+	}.join("")
+}
 
-	args.each{
-		double candidate = it.toDouble()
-		int sparkIndex = (int) ((candidate - min) / interval);
-		output+=sparks[sparkIndex]
-	}
-
-	println output
+if(args.length==0){
+	def example="23 45 9.98 23 76 34 9 1 53 65 23 54 1"
+    println "usage: \"spark "+example+"\" would result in " + sparkit(example.split(" "))
+}
+else {
+	println sparkit(args)
 }
